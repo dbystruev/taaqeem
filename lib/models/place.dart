ac907ed73393ac6ecce035f22e56b2d4d94b9c1f
Denis Bystruev <dbystruev@me.com>
2020-04-22 22:54:09 +0300

    Changed plans model

--- lib/models/place.dart
+++
@@ -1,7 +0,0 @@
-//
-//  lib/models/place.dart
-//
-//  Created by Denis Bystruev on 15/04/2020, updated 19/04/2020.
-//
-
-enum Place { food, industrial, office, residence }
--- lib/models/plan.dart
+++ lib/models/plan.dart
@@ -4,27 +4,25 @@
 //  Created by Denis Bystruev on 18/03/2020, updated 19/04/2020.
 //
 
-import 'package:taaqeem/models/place.dart';
-
 class Plan {
-  final String currency;
   final String description;
-  final int hours;
+  final String icon;
   final int id;
-  final Place place;
-  final int price;
+  final String image;
+  final String placeType;
   final String title;
+  final String subtitle;
 
   static int _maxId = 0;
   static int get maxId => _maxId;
 
   Plan(
     this.title, {
-    this.currency = 'AED',
-    this.hours = 0,
     this.description,
-    this.place,
-    this.price,
+    this.icon,
+    this.image,
+    this.placeType,
+    this.subtitle,
   }) : id = ++_maxId;
 
   void dispose() => _maxId -= id == _maxId ? 1 : 0;
@@ -32,9 +30,10 @@ class Plan {
   @override
   String toString() =>
       'Plan(\'$title\'' +
-      (currency == null ? '' : ', currency: \'$currency\'') +
       (description == null ? '' : ', description: \'$description\'') +
-      (place == null ? '' : ', place: "\'place\'') +
-      (price == null ? '' : ', price: \'$price\'') +
+      (icon == null ? '' : ', icon: "\'icon\'') +
+      (image == null ? '' : ', image: "\'image\'') +
+      (placeType == null ? '' : ', place: "\'place\'') +
+      (subtitle == null ? '' : ', subtitle: "\'subtitle\'') +
       ')';
 }
